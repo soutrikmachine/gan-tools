41e8adf5e2ea4fd5dd43ad98039ea02fbe03296a
Hannes De Meulemeester <hannesdm@users.noreply.github.com>
2019-02-07 23:13:08 +0100

    add matplotlib params to vis

--- core/vis.py
+++ core/vis.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+#default plt parameters
+matplotlib_params = {}
 
 def image_grid(array, columns):
     nr, height, width, channels = array.shape
@@ -12,9 +14,11 @@ def image_grid(array, columns):
     return result
 
 
-def show_gan_image_predictions(gan, nr, columns=8):
+def show_gan_image_predictions(gan, nr, columns=8, plt_params=None):
+    if plt_params is None:
+        plt_params = matplotlib_params
     images = gan.generate(nr)
     grid = image_grid(images, columns)
     grid = 0.5 * grid + 0.5
-    plt.imshow(np.squeeze(grid))
+    plt.imshow(np.squeeze(grid), **plt_params)
     plt.show()
