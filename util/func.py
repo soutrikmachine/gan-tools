f7231b9a673e98ec0486374b06af2d202ac4513b
Hannes De Meulemeester <hannesdm@users.noreply.github.com>
2019-03-12 20:12:12 +0100

    remove print in do_n_times

--- util/func.py
+++ util/func.py
@@ -6,7 +6,6 @@ def do_n_times(n, fun, aggregate_func=None, **fun_args):
         tup = fun(**fun_args)
         return_values.append(list(tup))
     return_values = list(map(list, zip(*return_values)))  # transpose
-    print(return_values)
     if aggregate_func is not None:
         aggregated_return_values = []
         for l in return_values:
